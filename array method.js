b910eada34061447bff0034fd1acbb467c56ede1
gaurialhat12 <gaurialhat15@gmail.com>
2026-03-11 12:31:17 +0000

    11

--- array method.js
+++ array method.js
@@ -3,3 +3,7 @@ console.log (name1.push("gauri"))
 
 const name1=["gauri","disha","nikita"];
 console.log ( name1.pop("disha"))
+
+const original = new Int16Array([100, 200, 300, 400, 500]);  
+const result = original.slice(1, 4);  
+console.log(result);  
