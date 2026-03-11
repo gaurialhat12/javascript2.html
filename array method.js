8e19ad736b4eeb205781a273c0154a3c54c0dc1d
gaurialhat12 <gaurialhat15@gmail.com>
2026-03-11 12:31:40 +0000

    11

--- array method.js
+++ array method.js
@@ -5,5 +5,5 @@ const name1=["gauri","disha","nikita"];
 console.log ( name1.pop("disha"))
 
 const original = new Int16Array([100, 200, 300, 400, 500]);  
-const result = original.slice(1, 4);  
+const result = original.slice(3, 4);  
 console.log(result);  
